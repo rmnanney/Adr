aa3e81094fc1e0a48eb976ef8f82a8ccbe866011
Ryan Nanney <serotonin@gmail.com>
2016-01-05 14:50:46 -0600

    Getting started on password resets.

--- src/Request.php
+++ src/Request.php
@@ -16,10 +16,11 @@ class Request extends \DOMDocument
     private $UserID;                //EG: 03
     private $Password;              //EG: 22225d7bs6e4bfdf71d5d5a1e912345
     private $PasswordFormat;        //EG: encrypted
-    private $NewPassword;           //EG:  22225d7bs6e4bfdf71d5d5a1e912345
-    private $NewPasswordFormat;     //EG:  encrypted
-    private $DeviceID;              //EG:  0C341E440634184 ... 644D593490332B405123456
-    private $DeviceIDFormat;        //EG:  encrypted
+    private $NewPassword;           //EG: 22225d7bs6e4bfdf71d5d5a1e912345
+    private $NewPasswordFormat;     //EG: encrypted
+    private $NewPasswordEncoded;    //EG: 5def5d7bs6e4bfdf71d5d5a1e9971fa4
+    private $DeviceID;              //EG: FF02A84F9B1D4314MVDEC5F98200FD57B0HLMEIDDHDCGN
+    private $DeviceIDFormat;        //EG: encoded
     private $ReportType;            //EG: XML
     private $xmlStr;
 
@@ -68,9 +69,11 @@ class Request extends \DOMDocument
         $comm->appendChild($doc->createElement('Host', $this->Host));
         $comm->appendChild($doc->createElement('Account', $this->Account));
         $comm->appendChild($doc->createElement('UserID', $this->UserID));
-        $comm->appendChild($doc->createElement('Password', $this->Password));
-        $comm->appendChild($doc->createElement('NewPassword', $this->NewPassword));
-        $comm->appendChild($doc->createElement('DeviceID', $this->DeviceID));
+        $comm->appendChild($doc->createElement('Password', $this->Password)->setAttribute('format', $this->PasswordFormat));
+        if(!empty($this->NewPassword)){
+            $comm->appendChild($doc->createElement('NewPassword', $this->NewPassword)->setAttribute('format', $this->NewPasswordFormat));
+        }
+        $comm->appendChild($doc->createElement('DeviceID', $this->DeviceID)->setAttribute('format', $this->DeviceIDFormat));
         $comm->appendChild($doc->createElement('ReportType', $this->ReportType));
         $adr->appendChild($comm);
 
@@ -86,6 +89,44 @@ class Request extends \DOMDocument
     public function save($params)
     {
         //some kind of file handler(s) to persist to the filesystem, and/or maybe a DB.
+    }
+
+    public function resetPassword(){
+        $this->NewPassword = $this->generatePassword();
+        $output = array();
+        exec("java -cp " . $this->adrJarFile . " adr.client.util.adrclienttool ".$this->NewPassword, $output);
+        $this->NewPasswordEncoded = trim(substr($output[0], 1+strpos($output[0], ":")));
+    }
+
+    private function generatePassword() {
+        /*
+        Upper case and Lowercase letters
+        One number and one symbol
+        8-10 characters
+        No more than two consecutive
+        identical characters
+        */
+        $password = array();
+        $upperCase = explode(',' , "A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z");
+        $lowerCase = explode(',' , "a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z");
+        $numbers = explode(',' , "0,1,2,3,4,5,6,7,8,9");
+        $special = explode(',' , "!,@,#,%,^,*");
+
+        shuffle($upperCase);
+        shuffle($lowerCase);
+        shuffle($numbers);
+        shuffle($special);
+
+        for ($i = 0; $i <= 3; $i++) $password[] = array_pop($upperCase);
+        for ($i = 0; $i <= 3; $i++) $password[] = array_pop($lowerCase);
+        $password[] = array_pop($numbers);
+        $password[] = array_pop($special);
+
+        shuffle($password);
+        $password = implode('', $password);
+
+        //profit :)
+        return $password;
     }
 
     /**
